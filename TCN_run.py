86155939fd3a189cb0f020f9e09b558569040dc1
Lorenzo Germini <lorenzo.germini@epfl.ch>
2020-12-14 15:25:30 +0100

    Update TCN_run.py
    still have to define parameters/hyberband upgrade and check indentation and spaces :)

--- TCN_run.py
+++ TCN_run.py
@@ -1,4 +1,3 @@
-#%%
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
@@ -8,60 +7,84 @@ from keras.utils import np_utils
 from kerastuner.tuners import RandomSearch
 from tcn import TCN, tcn_full_summary
 import Helpers.p_indicators as p_ind
-df = pd.read_csv('./Data/preprocessed.csv', index_col=0, parse_dates=True)
-df_numpy = df.to_numpy()
-N = len(df_numpy)
-n_train = int(N* 0.8)
-n_test = N - n_train
-n_classes = len(np.unique(df_numpy))
-dataY = df_numpy[:,0]
-dataY = np_utils.to_categorical(dataY)
-dataX = df_numpy[:,1:]
-Y_train = dataY[:n_train]
-Y_test = dataY[n_train:]
-memory=50
-dataX[n_train:,:memory].shape
-X_train = np.reshape(dataX[:n_train,:memory], (n_train, memory, 1))
-X_test = np.reshape(dataX[n_train:,:memory], (n_test, memory,1))
-X_train = X_train / float(n_classes)
-X_test = X_test / float(n_classes)
-i = Input(shape=(memory, 1))
-o = TCN(nb_filters = 6, kernel_size=9, nb_stacks=2, dilations=[2 ** i for i in range(10)], padding='causal', dropout_rate=0.5, activation='sigmoid', kernel_initializer='he_normal' )(i)
-o = Dense(Y_train.shape[1], activation='softmax')(o)
-model = Model(inputs=[i], outputs=[o])
-model.compile('adam','categorical_crossentropy', metrics=['accuracy'])
-tcn_full_summary(model, expand_residual_blocks=True)
-model.fit(X_train, Y_train, epochs=10)
+import LSTM.prepare_data as p_d
 
-prediction = model.predict(X_test, verbose=0)
-y_pred = np.argmax(prediction, axis=1)
-Y_test_val = np.argmax(Y_test,axis=1)
-p_ind.p_inds(Y_test_val,y_pred,'gggg')
-#%%
+if __name__ == "__main__":
+    # if code ran standalone, perform LSTM
+    
+    # filenames
+    #in_file_path = './Data/'
+    in_file_path = './Data/preprocessed.csv'
+    out_file_path = './Data/cleaned_data.csv'
+    # load the data
+    df = pd.read_csv(in_file_path,index_col=0,parse_dates=True)    
+    X_train, Y_train, X_test, Y_test=p_d(df, memory, valid_ratio=0.8)
 
-def build_model(hp):
-    i = Input(shape=(memory, 1))
-    o = TCN(nb_filters = hp.Int('nb_filters', min_value=32, max_value=512, step=32), kernel_size=hp.Int('kernel', min_value=2, max_value=10, step=1), nb_stacks=2, dilations=[1,2,4,8,16,32], padding='causal')(i)
-    #o = Dropout(0.5)(o)
-    o = Dense(Y_train.shape[1], activation='softmax')(o)
-    model = Model(inputs=[i], outputs=[o])
-    model.summary()
-#model.compile(optimizer=tf.keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])), 'categorical_crossentropy', metrics=['accuracy'])
-    model.compile('adam','categorical_crossentropy', metrics=['accuracy'])
-    return model
-
-print('Train...')
-
-tuner = RandomSearch(build_model,objective='val_accuracy',max_trials=5,executions_per_trial=3,directory='C:/Users/loren/OneDrive/Documenti/my_dir')
-
-tuner.search_space_summary()
-
-tuner.search(X_train,Y_train,epochs=5,validation_data=(X_test, Y_test))
-tuner.results_summary()
-print('ciao')
-best_model = tuner.get_best_models(num_models=2)
-#best_model.fit(X_train, Y_train, epochs=30)
-prediction = best_model.predict(X_test, verbose=0)
-y_pred = np.argmax(prediction, axis=1)
-Y_test_val = np.argmax(Y_test,axis=1)
-p_ind.p_inds(Y_test_val,y_pred,'g')
+    
+#define a  TCN model-building function. It takes an hp argument from which you can sample hyperparameters
+def tcn_predict(hyperparam_opt, history_window):
+    if hyperparam_opt==True:
+    # perform the tuning of teh parameters on the model and then return the output of the tuned model
+        def build_model(hp,history_window):
+            i = Input(shape=(history_window, 1))
+            o = TCN(nb_filters = hp.Int('nb_filters', min_value=32, max_value=512, step=32), kernel_size=hp.Int('kernel', min_value=2, max_value=10, step=1), nb_stacks=1, dilations=[1,2,4,8,16,32], padding='causal')(i
+            o = Dense(Y_train.shape[1], activation='softmax')(o)
+            model = Model(inputs=[i], outputs=[o])
+            #model.compile(optimizer=tf.keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])), 'categorical_crossentropy', metrics=['accuracy'])
+            model.compile('adam','categorical_crossentropy', metrics=['accuracy'])
+            tcn_full_summary(model, expand_residual_blocks=True)
+            return model
+       
+        #instiatate a tuner on a given model with the objective of optimizing val_accuracy usig random search/hyperband more efficient 
+        #def tune_model(build_model,):
+        #still have to adjust the parameter settings and some spaces :)
+        tuner = RandomSearch(build_model,objective='val_accuracy',max_trials=5,executions_per_trial=3,directory='..')
+        #get a search space summary
+        tuner.search_space_summary()
+        #start the tuning process
+        tuner.search(X_train,Y_train,epochs=5,validation_data=(X_test, Y_test))
+        
+        tuner.results_summary()
+        best_model = tuner.get_best_models(num_models=2)
+        
+        #run model
+        best_model.fit(X_train, Y_train, epochs=30)
+        
+        #predict
+        prediction = best_model.predict(X_test, verbose=0)
+        
+        #unencode the one hot encoded labels
+        y_pred = np.argmax(prediction, axis=1)
+        y_true = np.argmax(Y_test,axis=1)
+        #return predicted labels
+        return y_pred, y_true
+        
+    elif hyperparam_opt==False:
+       #directly return the otuput of the tuned model
+       def build_already_tuned_model(history_window):
+           i = Input(shape=(history_window, 1))
+           o = TCN(nb_filters = 6, kernel_size=9, nb_stacks=2, dilations=[2 ** i for i in range(10)], padding='causal', dropout_rate=0.5, activation='softmax', kernel_initializer='he_normal' )(i)
+           o = Dense(Y_train.shape[1], activation='softmax')(o)
+           model = Model(inputs=[i], outputs=[o])
+           model.compile('adam','categorical_crossentropy', metrics=['accuracy'])
+           tcn_full_summary(model, expand_residual_blocks=True)
+           return model
+           
+       #run model
+       model.fit(X_train, Y_train, epochs=5)
+        
+       #predict
+       prediction = model.predict(X_test, verbose=0)
+        
+       #unencode the one hot encoded labels
+       y_pred = np.argmax(prediction, axis=1)
+       y_true = np.argmax(Y_test,axis=1)
+       #return predicted labels
+       return y_pred, y_true           
+       
+       
+           
+          
+               
+               
+               
